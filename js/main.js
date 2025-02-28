11d7bb8a7bf8bbda427b7e25fd6db1eabadfa0cd
tvignes <tcvignes1@gmail.com>
2025-02-28 10:58:13 -0600

    work before class

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // JS scripts placed here
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.getElementById('offcanvas-menu');
+
+menuToggle.addEventListener('click', () => {
+  const expanded = menuToggle.getAttribute('aria-expanded') === 'true' || false;
+  menuToggle.setAttribute('aria-expanded',!expanded);
+  menu.classList.toggle('expanded');
+});
